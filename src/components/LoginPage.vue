e324e2a9b79ebc0fb9121be4429b828afb842b05
Mannyg448 <manny.g448@hotmail.com>
2023-03-22 15:04:10 -0500

    adding changes to create the login/logout inputs

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,3 +1,43 @@
+
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
+import { ref } from 'vue'
+import { useAuth } from '@/composables/useAuth'
+const { login, logout } = useAuth()
+
+const route = useRoute()
+const router = useRouter()
+
+
+const username = ref('')
+const password = ref('')
+
+ const logUserIn = async () => {
+    if (await login(username.value, password.value)) {
+      if (route.query.redirect) {
+        router.push(route.query.redirect)
+      } else {
+        router.push({ name: 'Home' })
+      }
+    } else {
+      logout()
+    }
+  }
+</script>
 <template>
-    <h1>Login Page</h1>
+    <form class="login-form" @submit.prevent="logUserIn">
+        <input v-model="username" type="text" placeholder="Username" />
+        <input v-model="password" type="password" placeholder="Password" />
+    <button type="submit" class=" bg-green-500 px-4 py-2">Log In</button>
+    </form>
 </template>
+
+<style scoped lang="postcss">
+.login-form {
+    @apply flex flex-col max-w-md mt-80 gap-4 mx-auto p-8 bg-white rounded-md shadow-lg; 
+
+    & input {
+        @apply text-xl px-4 ring-1 rounded-md ring-slate-300;
+    }
+}
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,47 +1,58 @@
 <script setup>
-import { ref } from 'vue'
-const brand = ref('🏨 Fake Company Directory')
+  import { ref } from 'vue'
+  import { useAuth } from '@/composables/useAuth'
+  const { isAuthenticated, logout, user } = useAuth()
+  const brand = ref('🏨 Fake Company Directory')
 </script>
-
-
-
 <template>
-   <nav class="bg-red-400">
-   <div class="wrapper">
-   <RouterLink :to="{ name: 'Home' }" class="brand">
-    <span class="brand-title">{{  brand }}</span>
-   </RouterLink>
-    <div class="menu">
-        <a href="#" class="menu-item">Department</a>
-        <a href="#" class="menu-item">Settings</a>
-        <a href="#" class="menu-login">Logout</a>
+  <nav>
+    <div class="wrapper">
+      <RouterLink :to="{ name: 'Home' }" class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </RouterLink>
+      <div class="menu">
+        <p v-show="isAuthenticated" class="px-2 py-4">
+          Welcome back
+          <strong>
+            <i>{{ user.name }}</i>
+          </strong>
+        </p>
+        <div v-if="isAuthenticated">
+          <RouterLink :to="{ name: 'Settings' }" href="#" class="menu-item">Settings</RouterLink>
+          <button class="menu-logout" @click="logout">Logout</button>
+        </div>
+        <div v-else>
+          <RouterLink :to="{ name: 'Login' }" href="#" class="menu-login">Login</RouterLink>
+        </div>
+      </div>
     </div>
-    </div>
-    </nav>
-
+  </nav>
 </template>
-
-
 <style scoped lang="postcss">
-nav {
-    @apply flex bg-purple-600 text-orange-400 h-20 ;
+  nav {
+    @apply flex h-20 bg-green-900 text-slate-200;
     .wrapper {
-        @apply flex  items-center justify-between container mx-auto w-full;
-        .brand {
-            &-title {
-                @apply text-2xl font-extrabold text-green-500;
-            }
-            
+      @apply container mx-auto flex w-full items-center justify-between;
+      .brand {
+        &-title {
+          @apply text-2xl font-bold text-yellow-500;
         }
-        .menu {
-            @apply flex gap-3;
-            &-item {
-                @apply hover:bg-green-500 hover:text-black px-1 py-1 rounded-md; 
-            }
-            &-login {
-                @apply hover:bg-red-500 bg-green-500 hover:text-orange-400 px-1 py-1 rounded-md text-black;
-            }
+      }
+      .menu {
+        @apply flex gap-2;
+        & div {
+          @apply py-2;
         }
+        &-item {
+          @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
+        }
+        &-login {
+          @apply rounded-md bg-green-500 px-4 py-2 text-red-100 hover:bg-green-700;
+        }
+        &-logout {
+          @apply mx-2 rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
+        }
+      }
     }
-}
+  }
 </style>
